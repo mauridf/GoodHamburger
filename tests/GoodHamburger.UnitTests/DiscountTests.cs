91097b09ddd9e005aa11784b515ffc8e5529546c
mauridf <mauridf@gmail.com>
2026-04-20 18:36:38 -0300

    feat: add unit tests for discount rules

---
+++ tests/GoodHamburger.UnitTests/DiscountTests.cs
@@ -0,0 +1,38 @@
+﻿using GoodHamburger.Domain.Entities;
+using GoodHamburger.Domain.Enums;
+
+namespace GoodHamburger.UnitTests;
+
+public class DiscountTests
+{
+    [Fact]
+    public void Should_Apply_Multiple_Combos_Correctly()
+    {
+        var order = new Order();
+
+        order.AddItem(new OrderItem(
+            Guid.NewGuid(),
+            "Burger",
+            5m,
+            MenuCategory.Sandwich,
+            2));
+
+        order.AddItem(new OrderItem(
+            Guid.NewGuid(),
+            "Drink",
+            2.5m,
+            MenuCategory.Drink,
+            2));
+
+        order.AddItem(new OrderItem(
+            Guid.NewGuid(),
+            "Fries",
+            2m,
+            MenuCategory.Side,
+            1));
+
+        Assert.Equal(17m, order.Subtotal);
+        Assert.Equal(3.03m, order.Discount);
+        Assert.Equal(13.97m, order.Total);
+    }
+}
